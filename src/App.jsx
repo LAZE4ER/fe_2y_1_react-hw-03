a9d18ac434caf34c69ad42ed5bac585750ca60d9
Danya <skrepecda998@gmail.com>
2023-12-12 19:46:54 +0200

    copy

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,9 @@ import {
   Stack,
 } from "@mui/material";
 import ContactList from "./components/ContactList";
+import CreateContactForm from "./components/CreateContactForm";
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -54,7 +50,7 @@ function App() {
         }}
       >
         <ContactList contacts={filterContacts()} />
-
+        <CreateContactForm />
         <h1>Find contacts by name</h1>
         <TextField type="text" onChange={(e) => setFilter(e.target.value)} />
       </Paper>
